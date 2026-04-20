88eb52134fe5226cda4ba810f96e939d83b888da
towfiqueemrose <towfiqueemrose@gmail.com>
2026-04-20 10:23:10 +0600

    year added in footer section

--- app/Model/BusinessSetting.php
+++ app/Model/BusinessSetting.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class BusinessSetting extends Model
 {
-
+    /**
+     * Get the dynamic value for specific settings.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getValueAttribute($value)
+    {
+        if ($this->type == 'company_copyright_text') {
+            return str_replace('{{year}}', date('Y'), preg_replace('/\b\d{4}\b/', date('Y'), $value));
+        }
+        return $value;
+    }
 }
