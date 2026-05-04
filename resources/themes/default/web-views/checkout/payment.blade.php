e8531bb6c627f25d27747999f3fe488dbb720a3a
towfiqueemrose <towfiqueemrose@gmail.com>
2026-05-04 12:43:03 +0600

    checkout payment loading issue fixed

--- resources/themes/default/web-views/checkout/payment.blade.php
+++ resources/themes/default/web-views/checkout/payment.blade.php
@@ -22,8 +22,8 @@
     </style>
 
     {{--stripe--}}
-    <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
-    <script src="https://js.stripe.com/v3/"></script>
+    <!-- <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script> -->
+    <!-- <script src="https://js.stripe.com/v3/"></script> -->
     {{--stripe--}}
 @endpush
 
